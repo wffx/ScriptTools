5b8fb6bf5cf916cca0d742fc431fee8df811c9e1
wffx <shuori2014@gmail.com>
2019-08-08 11:15:27 +0800

    little rectify on pdf-download

--- pdf_download/main.py
+++ pdf_download/main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import html
-import urllib.request
+from urllib import request,error
 import sys
-import re
+from urllib.parse import urljoin
 import string
 import wget
 
@@ -13,20 +13,29 @@ def main(argv):
         return None
 
     url = argv[1]
-    f = urllib.request.urlopen(url)
+    f = request.urlopen(url)
     res = f.read()
 
     html = BeautifulSoup(res, 'html.parser')
-    pat = re.compile(r'pdf')
+    #pat = re.compile(r'pdf')
     
     for tag in html.find_all("a"):       
         if tag['href'].find(".pdf") != -1 :
             st = tag["href"]
             if tag["href"][0] == '.':
                 st = st[2:len(st)]
-            st = url + st
-            wget.download(st)
-            print(st)
+                st = url + st
+            elif tag['href'].find("http") == -1:
+                #and tag['href'].find("www") != -1:
+                st = urljoin(url, st)
+            print(st)    
+            try:
+                 wget.download(st)
+            except error.HTTPError as e:
+                print(e.code)
+                pass
+            
+            continue
 
 if __name__ == "__main__":
     main(sys.argv)
